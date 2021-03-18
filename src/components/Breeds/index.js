54f47b8f01fc1fefa4e89487bcf781b42c5cf0e5
RauboLuk <rauboluk@gmail.com>
2021-03-18 21:25:53 +0100

    styling breeds imgs

--- src/components/Breeds/index.js
+++ src/components/Breeds/index.js
@@ -9,12 +9,75 @@ const Wrapper = styled.div`
   background: #e3e1dc;
 `;
 
+const ImagesSection = styled.div`
+  display: flex;
+  gap: 51px;
+  justify-content: center;
+`;
+
+const Item = styled.div`
+  width: 220px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ImgWrapper = styled.div`
+  height: 220px;
+  width: 220px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Img = styled.img`
+  max-height: 220px;
+  max-width: 220px;
+  border-radius: 24px;
+`;
+
+const Desc = styled.p`
+  font-family: Montserrat;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 22px;
+  letter-spacing: 0em;
+  text-align: left;
+  word-wrap: anywhere;
+`;
+
 const Breeds = () => {
   return (
     <Wrapper>
       <p>Most Searched Breeds</p>
       <p>66+ Breeds For you to discover</p>
       <p>SEE MORE</p>
+      <ImagesSection>
+        <Item>
+          <ImgWrapper>
+            <Img src="https://via.placeholder.com/220" />
+          </ImgWrapper>
+          <Desc>Bengal</Desc>
+        </Item>
+        <Item>
+          <ImgWrapper>
+            <Img src="https://via.placeholder.com/220x400" />
+          </ImgWrapper>
+          <Desc>Norwegian Forest Cat</Desc>
+        </Item>
+        <Item>
+          <ImgWrapper>
+            <Img src="https://via.placeholder.com/50" />
+          </ImgWrapper>
+          <Desc>Bengal</Desc>
+        </Item>
+        <Item>
+          <ImgWrapper>
+            <Img src="https://via.placeholder.com/270x100" />
+          </ImgWrapper>
+          <Desc>BenNorwegianNorwegianNorwegiangal</Desc>
+        </Item>
+      </ImagesSection>
     </Wrapper>
   );
 };
