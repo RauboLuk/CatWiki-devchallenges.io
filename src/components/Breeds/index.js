d856cf6e9e74e00bd4195ed29ef8a54db2c08bd1
RauboLuk <rauboluk@gmail.com>
2021-03-18 22:36:44 +0100

    styled breeds

--- src/components/Breeds/index.js
+++ src/components/Breeds/index.js
@@ -1,37 +1,102 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  width: 100%;
-  height: 686.4px;
   margin-top: -40px;
-  padding-top: 40px;
+  padding: 80px 108px 108px 108px;
+  border-radius: 0px 0px 42px 42px;
 
   background: #e3e1dc;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+    "title ."
+    "bigText more"
+    "images images";
+  gap: 46px;
+
+  & * {
+    margin: 0;
+    color: #291507;
+  }
+`;
+
+const Title = styled.p`
+  grid-area: title;
+  font-family: Montserrat;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 22px;
+  letter-spacing: 0em;
+  text-align: left;
+
+  &:after {
+    content: "";
+    margin-top: 8px;
+    display: block;
+    width: 59.18px;
+    height: 3.21px;
+    left: 204px;
+    top: 707.19px;
+
+    background: #4d270c;
+    border-radius: 77px;
+  }
+`;
+
+const BigText = styled.p`
+  grid-area: bigText;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 59px;
+`;
+
+const More = styled.p`
+  grid-area: more;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 22px;
+
+  text-align: right;
+  align-self: end;
+
+  color: rgba(41, 21, 7, 0.6);
 `;
 
 const ImagesSection = styled.div`
-  display: flex;
+  grid-area: images;
+  display: grid;
   gap: 51px;
+  grid-template-columns: repeat(4, 1fr);
   justify-content: center;
 `;
 
 const Item = styled.div`
-  width: 220px;
   display: flex;
+  gap: 20px;
   flex-direction: column;
 `;
 
 const ImgWrapper = styled.div`
-  height: 220px;
-  width: 220px;
+  max-height: 220px;
+  max-width: 100%;
+
+  ${"" /* doesn't work on ff */}
+  aspect-ratio: 1 / 1;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
 const Img = styled.img`
-  max-height: 220px;
-  max-width: 220px;
+  max-height: 100%;
+  max-width: 100%;
   border-radius: 24px;
 `;
 
@@ -49,9 +114,9 @@ const Desc = styled.p`
 const Breeds = () => {
   return (
     <Wrapper>
-      <p>Most Searched Breeds</p>
-      <p>66+ Breeds For you to discover</p>
-      <p>SEE MORE</p>
+      <Title>Most Searched Breeds</Title>
+      <BigText>66+ Breeds For you to discover</BigText>
+      <More>SEE MORE</More>
       <ImagesSection>
         <Item>
           <ImgWrapper>
