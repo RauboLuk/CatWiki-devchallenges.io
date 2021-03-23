9d23a2a76c471e726b9b1ec1eb2918740d9336eb
RauboLuk <rauboluk@gmail.com>
2021-03-23 23:58:32 +0100

    styling details imgs

--- client/src/components/BreedDetails/components/Photos.js
+++ client/src/components/BreedDetails/components/Photos.js
@@ -1,8 +1,48 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+margin: 8vh 0 15vh 0;
+`;
+
+const Desc = styled.p`
+  font-family: Montserrat;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 44px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
+
+const ImgsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(auto, 300px);
+  justify-items: center;
+  align-items: center;
+  justify-content: stretch;
+  align-content: stretch;
+  gap: 2vw;
+`;
+
+const Img = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
+
 const Photos = () => {
   return (
-    <div>
-      xd
-    </div>
+    <Wrapper>
+      <Desc>Other photos</Desc>
+      <ImgsWrapper>
+        <Img src="https://via.placeholder.com/380x380" alt="" />
+        <Img src="https://via.placeholder.com/580x180" alt="" />
+        <Img src="https://via.placeholder.com/180x580" alt="" />
+        <Img src="https://via.placeholder.com/320x350" alt="" />
+        <Img src="https://via.placeholder.com/100" alt="" />
+        <Img src="https://via.placeholder.com/450x450" alt="" />
+      </ImgsWrapper>
+    </Wrapper>
   );
 };
 
