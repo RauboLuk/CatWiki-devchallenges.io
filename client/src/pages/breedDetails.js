961bc1ba136e7ce97096269d1afd0fd4a131ecbc
RauboLuk <rauboluk@gmail.com>
2021-04-03 23:43:53 +0200

    hotfix

--- client/src/pages/breedDetails.js
+++ client/src/pages/breedDetails.js
@@ -36,7 +36,7 @@ const BreedDetails = () => {
   }, [loading, imgLoading, imageData, data, visitCalled, addCatVisit]);
 
   if (loading || imgLoading) return <LoadingBar />;
-  if (true || error || !data.getBreedWithImgUrls.success) return <ErrorMsg message={`Ooops, unknown error.`} />;
+  if (error || !data.getBreedWithImgUrls.success) return <ErrorMsg message={`Ooops, unknown error.`} />;
 
   console.log(data.getBreedWithImgUrls);
   console.log("imageData", imageData?.getImageUrl);
